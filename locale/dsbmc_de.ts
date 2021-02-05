b476052dd2e1bc7534e0271d3e3a4c65b1443cad
Marcel <mk@freeshell.de>
2021-02-05 15:54:55 +0100

    Update

--- locale/dsbmc_de.ts
+++ locale/dsbmc_de.ts
@@ -4,148 +4,154 @@
 <context>
     <name>MainWin</name>
     <message>
-        <location filename="../src/mainwin.cpp" line="84"/>
+        <location filename="../src/mainwin.cpp" line="94"/>
         <source>DSBMC - DSBMD Client</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="239"/>
+        <location filename="../src/mainwin.cpp" line="162"/>
+        <source>Show/hide window</source>
+        <translation>Zeige/verberge Fenster</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="255"/>
         <source>Storage device %1 added</source>
         <translation>Speichermedium %1 hinzugefügt</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="247"/>
+        <location filename="../src/mainwin.cpp" line="263"/>
         <source>Storage device</source>
         <translation>Speichermedium</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="279"/>
+        <location filename="../src/mainwin.cpp" line="295"/>
         <source>Couldn&apos;t mount %1: %2</source>
         <translation>Konnte %1 nicht einhängen: %2</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="291"/>
+        <location filename="../src/mainwin.cpp" line="307"/>
         <source>Couldn&apos;t unmount %1: %2</source>
         <translation>Konnte %1 nicht aushängen: %2</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="305"/>
+        <location filename="../src/mainwin.cpp" line="321"/>
         <source>Couldn&apos;t eject %1: %2</source>
         <translation>Konnte %1 nicht auswerfen: %2</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="320"/>
+        <location filename="../src/mainwin.cpp" line="336"/>
         <source>Couldn&apos;t start %1: %2</source>
         <translation>Konnte %1 nicht starten: %2</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="369"/>
-        <location filename="../src/mainwin.cpp" line="447"/>
+        <location filename="../src/mainwin.cpp" line="385"/>
+        <location filename="../src/mainwin.cpp" line="463"/>
         <source>Opening %1. Please wait</source>
         <translation>Öffne %1. Bitte warten</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="382"/>
+        <location filename="../src/mainwin.cpp" line="398"/>
         <source>Mounting %1. Please wait</source>
         <translation>Hänge %1 ein. Bitte warten</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="518"/>
+        <location filename="../src/mainwin.cpp" line="534"/>
         <source>Device Busy</source>
         <translation>Gerät blockiert</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="521"/>
+        <location filename="../src/mainwin.cpp" line="537"/>
         <source>%1 is busy. Would you like to forcefully unmount/eject it?</source>
         <translation>%1 is blockiert. Möchten Sie das Aushängen/Auswerfen erzwingen?</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="523"/>
+        <location filename="../src/mainwin.cpp" line="539"/>
         <source>Force</source>
         <translation>Erzwingen</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="524"/>
+        <location filename="../src/mainwin.cpp" line="540"/>
         <source>Cancel</source>
         <translation>Abbrechen</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="537"/>
+        <location filename="../src/mainwin.cpp" line="553"/>
         <source>&amp;Ok</source>
         <translation>&amp;Ok</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="538"/>
+        <location filename="../src/mainwin.cpp" line="554"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Abbrechen</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="543"/>
+        <location filename="../src/mainwin.cpp" line="559"/>
         <source>Set reading speed</source>
         <translation>Setze Lesegeschwindigkeit</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="550"/>
+        <location filename="../src/mainwin.cpp" line="566"/>
         <source>Set the reading speed of %1</source>
         <translation>Setze Lesegeschwindigkeit von %1</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="286"/>
-        <location filename="../src/mainwin.cpp" line="387"/>
+        <location filename="../src/mainwin.cpp" line="302"/>
+        <location filename="../src/mainwin.cpp" line="403"/>
         <source>Unmounting %1. Please wait</source>
         <translation>Hänge %1 aus. Bitte warten</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="300"/>
-        <location filename="../src/mainwin.cpp" line="392"/>
+        <location filename="../src/mainwin.cpp" line="316"/>
+        <location filename="../src/mainwin.cpp" line="408"/>
         <source>Ejecting %1. Please wait</source>
         <translation>Werfe %1 aus. Bitte warten</translation>
     </message>
     <message>
         <location filename="../src/mainwin.cpp" line="159"/>
-        <location filename="../src/mainwin.cpp" line="489"/>
+        <location filename="../src/mainwin.cpp" line="505"/>
         <source>&amp;Quit</source>
         <translation>&amp;Beenden</translation>
     </message>
     <message>
         <location filename="../src/mainwin.cpp" line="160"/>
-        <location filename="../src/mainwin.cpp" line="490"/>
+        <location filename="../src/mainwin.cpp" line="506"/>
         <source>&amp;Preferences</source>
         <translation>&amp;Einstellungen</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="400"/>
+        <location filename="../src/mainwin.cpp" line="416"/>
         <source>Setting reading speed of %1</source>
         <translation>Setze Lesegeschindigkeit von %1</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="466"/>
+        <location filename="../src/mainwin.cpp" line="482"/>
         <source>DSBMD just shut down</source>
         <translation>DSBMD hat seinen Dienst beendet</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="472"/>
+        <location filename="../src/mainwin.cpp" line="488"/>
         <source>Lost connection to DSBMD</source>
         <translation>Verbindung zu DSBMD verloren</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="494"/>
+        <location filename="../src/mainwin.cpp" line="510"/>
         <source>&amp;File</source>
         <translation>&amp;Datei</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="504"/>
+        <location filename="../src/mainwin.cpp" line="520"/>
         <source>An error occured
 </source>
-        <translation>Ein Fehler ist Aufgetreten</translation>
+        <translation>Ein Fehler ist Aufgetreten
+</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="505"/>
+        <location filename="../src/mainwin.cpp" line="521"/>
         <source>Error</source>
         <translation>Fehler</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="509"/>
+        <location filename="../src/mainwin.cpp" line="525"/>
         <source>Ok</source>
         <translation>Ok</translation>
     </message>
@@ -281,109 +287,107 @@
 <context>
     <name>Preferences</name>
     <message>
-        <location filename="../src/preferences.cpp" line="39"/>
+        <location filename="../src/preferences.cpp" line="41"/>
         <source>&amp;Ok</source>
         <translation>&amp;Ok</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="40"/>
+        <location filename="../src/preferences.cpp" line="42"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Abbrechen</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="45"/>
+        <location filename="../src/preferences.cpp" line="47"/>
         <source>Preferences</source>
         <translation>Einstellungen</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="47"/>
+        <location filename="../src/preferences.cpp" line="49"/>
         <source>General settings</source>
         <translation>Allgemeine Einstellungen</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="48"/>
+        <location filename="../src/preferences.cpp" line="50"/>
         <source>Commands</source>
         <translation>Befehle</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="145"/>
+        <location filename="../src/preferences.cpp" line="192"/>
+        <source>Tray Icon Theme</source>
+        <translation>Tray-Symbolthema</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="193"/>
+        <source>Ignore Devices</source>
+        <translation>Ignoriere Geräte</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="194"/>
+        <source>Behavior</source>
+        <translation>Verhalten</translation>
+    </message>
+    <message>
+        <location filename="../src/preferences.cpp" line="197"/>
         <source>Hide main window after opening a device</source>
         <translation>Hauptfenster nach dem Öffnen eines Geräts verstecken</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="147"/>
+        <location filename="../src/preferences.cpp" line="199"/>
         <source>Show notification when a device was added</source>
         <translation>Zeige Benachrichtigung, wenn ein Gerät hinzugefügt worden ist</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="149"/>
+        <location filename="../src/preferences.cpp" line="201"/>
         <source>Show main window when a device was added</source>
         <translation>Zeige Hauptfenster, wenn ein Gerät hinzugefügt worden ist</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="165"/>
-        <source>Browse files</source>
-        <translation>Datei öffnen</translation>
-    </message>
-    <message>
-        <location filename="../src/preferences.cpp" line="169"/>
-        <source>Tray icon:</source>
-        <translation>Tray-Icon:</translation>
-    </message>
-    <message>
-        <location filename="../src/preferences.cpp" line="173"/>
-        <source>Ignore devices and
-mount points:</source>
-        <translation>Ignoriere Geräte und
-Mount points:</translation>
-    </message>
-    <message>
-        <location filename="../src/preferences.cpp" line="176"/>
+        <location filename="../src/preferences.cpp" line="223"/>
         <source>Example: &lt;tt&gt;/dev/da0s1, EFISYS, /var/run/user/1001/gvfs&lt;/tt&gt;</source>
         <translation>Beispiel:&lt;tt&gt;/dev/da0s1, EFISYS, /var/run/user/1001/gvfs&lt;/tt&gt;</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="201"/>
+        <location filename="../src/preferences.cpp" line="254"/>
         <source>Use %d and %m as placeholders for the device and mount point, respectively</source>
         <translation>Benutzen Sie %d und %m als Platzhalter für das Gerät, bzw. den Mount point</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="212"/>
+        <location filename="../src/preferences.cpp" line="265"/>
         <source>Filemanager:</source>
         <translation>Dateimanager:</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="216"/>
+        <location filename="../src/preferences.cpp" line="269"/>
         <source>Play DVDs with:</source>
         <translation>Spiele DVDs mit:</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="220"/>
+        <location filename="../src/preferences.cpp" line="273"/>
         <source>Play Audio CDs with:</source>
         <translation>Spiele Audio-CDs mit:</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="224"/>
+        <location filename="../src/preferences.cpp" line="277"/>
         <source>Play VCDs with:</source>
         <translation>Spiele VCDs mit:</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="228"/>
+        <location filename="../src/preferences.cpp" line="281"/>
         <source>Play SVCDs with:</source>
         <translation>Spiele SVCDs mit:</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="238"/>
+        <location filename="../src/preferences.cpp" line="291"/>
         <source>Autoplay</source>
         <translation>Automatisch abspielen</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="254"/>
+        <location filename="../src/preferences.cpp" line="310"/>
         <source>Open Image</source>
         <translation>Bilddatei öffnen</translation>
     </message>
     <message>
-        <location filename="../src/preferences.cpp" line="255"/>
+        <location filename="../src/preferences.cpp" line="311"/>
         <source>Image Files (*.png *.svg *.xpm)</source>
         <translation>Bilddateien (*.png *.svg *.xpm)</translation>
     </message>
